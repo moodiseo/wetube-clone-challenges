af161c812e8fc8cb27c420d98855a7c062462581
김다빈 <135933231+slqkealr@users.noreply.github.com>
2023-11-22 21:37:26 +0900

    Assignment-3

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 import express from "express";
 const app = express();
 
+const urlLogger = (req, res, next) => {
+  console.log("Path: ", req.path);
+  next();
+};
+const timeLogger = (req, res, next) => {
+  const date = new Date();
+  console.log(
+    `Time: ${date.getFullYear()}.${date.getMonth()}.${date.getDate()}`
+  );
+  next();
+};
+const securityLogger = (req, res, next) => {
+  if (req.protocol === "https") {
+    console.log("Secure");
+  } else {
+    console.log("Insecure");
+  }
+  next();
+};
+const protectorLogger = (req, res, next) => {
+  if (req.path === "/protected") {
+    return res.send("<h1>Forbidden<h1>");
+  }
+  next();
+};
 const handleHome = (req, res) => {
-  return res.send("handleHome");
+  return res.send("Home");
 };
-const handleAbout = (req, res) => {
-  return res.send("handleAbout");
-};
-const handleContact = (req, res) => {
-  return res.send("handleContact");
-};
-const handleLogin = (req, res) => {
-  return res.send("handleLogin");
-};
-
+app.use(urlLogger, timeLogger, securityLogger, protectorLogger);
 app.get("/", handleHome);
-app.get("/about", handleAbout);
-app.get("/contact", handleContact);
-app.get("/login", handleLogin);
 
 const handleListening = () => console.log("Server Listening");
 app.listen(3000, handleListening);
