9cdf3f46448150498f4bbf2de8b28b4cf03f5867
moodiseo <moodiseo@gmail.com>
2023-11-24 15:54:12 +0900

    Assignment-5

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { join, login, editProfile } from "../controllers/userController";
+import { join, login } from "../controllers/userController";
 import { home, trending, newStory } from "../controllers/storiesController";
 
 const globalRouter = express.Router();
@@ -9,6 +9,5 @@ globalRouter.get("/join", join);
 globalRouter.get("/login", login);
 globalRouter.get("/trending", trending);
 globalRouter.get("/new", newStory);
-globalRouter.get("/edit-profile", editProfile);
 
 export default globalRouter;
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { userSee, userHome } from "../controllers/userController";
+import { userSee, userHome, editProfile } from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/", userHome);
 userRouter.get("/:id(\\d+)", userSee);
+userRouter.get("/edit-profile", editProfile);
 
 export default userRouter;
